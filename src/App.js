dd5c4a2978b7e7d114097142f15d07eb2f264b43
Dalton Tewanger <daltontewanger@yahoo.com>
2024-07-25 20:44:02 -0700

    Added context for defining the current user, updated app.js accordingly

--- src/App.js
+++ src/App.js
@@ -1,29 +1,31 @@
 // import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 // import NavBar from './Components/NavBar';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import SignIn from './Components/SignIn';
-import SignUp from './Components/SignUp';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import CurrentUserProvider from "./Contexts/CurrentUser";
+import SignIn from "./Components/SignIn";
+import SignUp from "./Components/SignUp";
 // import AppAppBar from './Components/AppAppBar';
-import LandingPage from './Components/LandingPage';
-import DeveloperView from './Components/DeveloperView';
-import RecruiterView from './Components/RecruiterView';
-import Footer from './Components/Footer';
-
+import LandingPage from "./Components/LandingPage";
+import DeveloperView from "./Components/DeveloperView";
+import RecruiterView from "./Components/RecruiterView";
+import Footer from "./Components/Footer";
 
 function App() {
   return (
     <div className="App">
-      <Router>
-        <Routes>
-          <Route path="/" element={<LandingPage />} />
-          <Route path="/signin" element={<SignIn />} />
-          <Route path="/signup" element={<SignUp />} />
-          <Route path="/developer" element={<DeveloperView />} />
-          <Route path="/recruiter" element={<RecruiterView />} />
-        </Routes>
-        <Footer />
-      </Router>
+      <CurrentUserProvider>
+        <Router>
+          <Routes>
+            <Route path="/" element={<LandingPage />} />
+            <Route path="/signin" element={<SignIn />} />
+            <Route path="/signup" element={<SignUp />} />
+            <Route path="/developer" element={<DeveloperView />} />
+            <Route path="/recruiter" element={<RecruiterView />} />
+          </Routes>
+          <Footer />
+        </Router>
+      </CurrentUserProvider>
     </div>
   );
 }
