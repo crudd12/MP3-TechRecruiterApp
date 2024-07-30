d4b546451cbacc7c1fa768a9f6339a8663fad913
Courtney <crudd12@gmail.com>
2024-07-30 06:29:26 -0700

    Update DeveloperList.js

--- src/Components/DeveloperList.js
+++ src/Components/DeveloperList.js
@@ -27,8 +27,8 @@ function DeveloperList() {
       try {
         // fetch all developers if no search input or filter by language
         const url = searchTerm
-          ? `https://techrecruiter.onrender.com/recruiter/languages?language=${searchTerm}`
-          : 'https://techrecruiter.onrender.com/recruiter/languages';
+          ? `https://techrecruiterapi.onrender.com/recruiter/languages?language=${searchTerm}`
+          : 'https://techrecruiterapi.onrender.com/recruiter/languages';
           
         const response = await fetch(url);
         if (!response.ok) {
